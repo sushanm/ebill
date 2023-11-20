d77f7794c2c46e77935e258346da41243ef41b7f
Sushan M <91654089+Sushan-M@users.noreply.github.com>
2023-11-20 19:09:59 +0530

    soring of data

--- src/components/AddStock.js
+++ src/components/AddStock.js
@@ -28,6 +28,11 @@ function AddStock({ saleMode }) {
             }, 1000)
         }
         else {
+            data.sort(function(a, b) {
+                var textA = a.name.toUpperCase();
+                var textB = b.name.toUpperCase();
+                return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+            });
             setProducts(data);
             setProductsForSearch(data)
             SetTotalFiltedCount(data.length)
